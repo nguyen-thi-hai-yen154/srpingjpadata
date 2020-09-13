16f5228f0be9aea2ff722149a58274faa2c32c81
tranvancanh <tranvancanh981@gmail.com>
2020-09-13 16:34:57 +0900

    branch of canh lan 2

---
+++ src/main/java/com/jpa/demospringjpa/model/MCategory.java
@@ -0,0 +1,55 @@
+package com.jpa.demospringjpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "m_category")
+public class MCategory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Long categoryId;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+	
+	@Column(name = "description")
+	private String description;
+
+	public MCategory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
