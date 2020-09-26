8209693c05219935461a5df6fcfec6fe929d9046
nguyen-thi-hai-yen154 <yenkhtn154@gmail.com>
2020-09-26 09:38:54 +0900

    add login , logout, registration function

---
+++ src/main/java/com/jpa/demospringjpa/service/UserService.java
@@ -0,0 +1,47 @@
+package com.jpa.demospringjpa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.jpa.demospringjpa.model.Role;
+import com.jpa.demospringjpa.model.User;
+import com.jpa.demospringjpa.respository.RoleRepository;
+import com.jpa.demospringjpa.respository.UserRepository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User findUserByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+}
