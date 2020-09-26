7471c9854b897a55a1fc20485f3bed1308a51dea
tranvancanh <tranvancanh981@gmail.com>
2020-09-26 10:40:54 +0900

    fix get set

--- src/main/java/com/jpa/demospringjpa/model/Role.java
+++ src/main/java/com/jpa/demospringjpa/model/Role.java
@@ -21,5 +21,19 @@ public class Role {
     private int id;
     @Column(name = "role")
     private String role;
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 
+    
 }
